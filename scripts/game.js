34817af10c5a2291e1838c9e78620710ea6202c9
Oliver Fei <oliverf1996@hotmail.ca>
2013-11-08 11:10:09 -0500

    updated GUI

--- scripts/game.js
+++ scripts/game.js
@@ -1,33 +1,51 @@
 			var c;
 			var ctx;
+			
 			var cStatic;
 			var ctxStatic;
-			var score;
+			
+			var bg;
+			var ctxbg;
+
+			var score=0;
 			var answer;
-			var x=20;
-			var y=0;
+			var userAnswer;
+			var x=-50;
+			var y=45;
 			var intervalId;
-        		var customer = new Image();
-        		customer.src = "images/redcircle.png";
-        		var table = new Image();
-        		table.src= "images/KEY_Table_sprite.png";
+			var counter=0;
+			var numDay=1;
+
+        	var table = new Image();
+        	table.src= "images/KEY_Table_sprite.png";        	
+			var customer = new Image();
+        	customer.src = "images/redcircle.png";
 			
 			$(function(){
+				bg=document.getElementById("bg");
+				ctxbg = bg.getContext("2d");
+
+				table.onload = function(){
+					ctxbg.drawImage(table,200,200,100,50);
+				};
+	
 				c=document.getElementById("myCanvas");
 				ctx = c.getContext("2d");
+				
 				cStatic = document.getElementById("staticCanvas");
 				ctxStatic = cStatic.getContext("2d");
-				score = 0;
 				ctxStatic.font = "30px Arial";
-				ctxStatic.fillText("Score: " + score, 20, 20);
+				ctxStatic.fillText("Score: " + score, 20, 30);
+				ctxStatic.fillText("Day " + numDay, 20, 61);
 				ctx.font="30px Arial";
 				move1();
 				
 			});
 
-			function updateScore() {
-			    ctxStatic.clearRect(10, 10, 200, 40);
-			    ctxStatic.fillText("Score: " + score, 20, 20);
+			function update() {
+			    ctxStatic.clearRect(0, 0, 200, 80);
+			    ctxStatic.fillText("Score: " + score, 20, 30);
+				ctxStatic.fillText("Day " + numDay, 20, 61);
 			}
 			
 			function getRandomQuestion(){
@@ -60,17 +78,33 @@
 				return question;	
 			}
 			
+			function restart(){
+				counter=0;
+				numDay=1;
+				score=0;
+				update();
+			}
+			
 			function processAnswer(){
-				var userAnswer=document.getElementById("userAnswer").value;
+
 				var answerString=answer+"";
 				if(parseFloat(userAnswer).toFixed(2)==parseFloat(answerString).toFixed(2)){
 					score+=1;
+				}
+				counter+=1;
+				if(counter%5==0){
+					if(numDay===2){
+						alert("GAME OVER");
+						restart();
+					}else{
+						numDay+=1;
+					}
 				}
 				reset();
 			}
 			
 			function generateQuestion(x,y){
-				ctx.fillText(getRandomQuestion(),x,y);
+				ctxStatic.fillText(getRandomQuestion(),x,y);
 			}
 			
 			function newCustomer()
@@ -88,7 +122,6 @@
                         		x+=1;
                         		ctx.clearRect(0,0, c.width, c.height);
                         		ctx.drawImage(customer, x, y, 50, 50);
-                        		ctx.drawImage(table, 200 ,200, 100, 50);
                         		if(x==150)
                         		{
                                 		clearInterval(intervalId);
@@ -106,7 +139,6 @@
                         		y+=1;
                         		ctx.clearRect(0,0, c.width, c.height);
                         		ctx.drawImage(customer, x, y, 50, 50);
-                        		ctx.drawImage(table, 200 ,200, 100, 50);
                         		if(y==200)
                         		{
                                 		clearInterval(intervalId);
@@ -117,11 +149,10 @@
 			
 			function reset()
         		{
-                		x=20;
-                		y=0;
+                		x=-50;
+                		y=45;
                 		ctx.clearRect(0,0, c.width, c.height);
-                		ctx.drawImage(table, 200, 200, 100, 50);
-                		updateScore();
+                		update();
                 		var rand= Math.floor(Math.random()*3+1);
                 		var t= setTimeout(function(){newCustomer()}, rand*1000);
         		}
