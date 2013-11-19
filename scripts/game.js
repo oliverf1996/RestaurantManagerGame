24bc506261091729e596fd7d263993b2b6e88000
DackJing <jacktcot5@hotmail.com>
2013-11-19 10:05:09 -0500

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -32,8 +32,7 @@
 			//customer.img.src=customer.src;
 			var customer = new Image();
         	customer.src = "images/redcircle.png";
-			var waiter = new Image();
-			waiter.src= "images/bluecircle.png";
+			
 			var chef = new Image();
 			chef.src="images/whitecircle.png";
 			
@@ -86,40 +85,7 @@
 				clockId=setTimeout(updateClock,1000);
 				
 			}
-			function getRandomQuestion(){
-				var operation;
-				var firstInteger;
-				var secondInteger;
-				var operationSelector;
-				
-				firstInteger=Math.floor((Math.random()*12)+1);
-				secondInteger=Math.floor((Math.random()*12)+1);
-				
-				operatorSelector=Math.floor((Math.random()*4));
-				switch(operatorSelector){				
-					case 0: operation="+";
-							answer=firstInteger+secondInteger;
-					break;
-				    case 1: operation = "-";
-				            var temp = firstInteger;
-				            if (firstInteger < secondInteger) {
-				                firstInteger = secondInteger;
-				                secondInteger = temp;
-				            }
-							answer=firstInteger-secondInteger;
-					break;
-					case 2: operation="*";
-							answer=firstInteger*secondInteger;
-					break;
-					case 3: operation="/";
-					        answer = firstInteger;
-					        firstInteger*=secondInteger;
-					break;
-				}
-				
-				question = "  " + firstInteger + operation + secondInteger + "=";
-				return question;
-			}
+			
 			
 			function restart(){
 				counter=0;
@@ -128,31 +94,6 @@
 				update();
 			}
 			
-			function processAnswer(){
-			    setButtonsDisabled(true);
-			    userAnswer = document.getElementById("questionDisplay").value.substring(question.length);
-				var answerString=answer+"";
-				if(parseFloat(userAnswer).toFixed(2)==parseFloat(answerString).toFixed(2)){
-					score+=1;
-				}
-				/*
-				counter+=1;
-				if(counter%5==0){
-					if(numDay===2){
-						alert("GAME OVER");
-						restart();
-					}else{
-						numDay+=1;
-					}
-				}*/
-				reset();
-			}
-			
-			function generateQuestion(){
-			    document.getElementById("questionDisplay").value = getRandomQuestion();
-			    setButtonsDisabled(false);
-			}
-            
 			function enterDigit(n) {
 			    if (document.getElementById("questionDisplay").value.length < 15) {
 			        document.getElementById("questionDisplay").value = document.getElementById("questionDisplay").value + "" + n;
@@ -168,88 +109,18 @@
                 		x+=1;
 						ctx.clearRect(0,0, c.width, c.height);
 						ctx.drawImage(customer, x, y, 50, 50);        
-						ctx.drawImage(waiter, x2, y2, 75, 75);	
+						
 						ctx.drawImage(chef, x3,y3, 50, 50);						
 						var test=setTimeout(newCustomer,10);
 						
 						if(x==30)		
 						{        
 							clearInterval(test);        
-							animateWaiter1();       
+							animateCustomer1();       
 						}	 
 						
         		}
 
-					
-			function animateWaiter1()
-			{
-				part1();
-				function part1()
-				{
-					y2-=1;
-					ctx.clearRect(0,0, c.width, c.height);
-					ctx.drawImage(customer, x, y, 50, 50); 
-					ctx.drawImage(waiter, x2, y2, 75, 75);
-					ctx.drawImage(chef, x3,y3, 50, 50);
-					var test=setTimeout(part1,10);
-					if(y2==50)
-					{
-						clearInterval(test);
-						part2();
-					}
-				}
-				function part2()
-				{
-					x2-=1;
-					ctx.clearRect(0,0, c.width, c.height);
-					ctx.drawImage(customer, x, y, 50, 50); 
-					ctx.drawImage(waiter, x2, y2, 75, 75);
-					
-					ctx.drawImage(chef, x3,y3, 50, 50);
-					var test=setTimeout(part2,10);
-					if(x2==30)
-					{
-						clearInterval(test);
-						animateCustomer1();
-						animateWaiter2();
-					}
-				
-				}
-			}
-			function animateWaiter2()
-			{
-				part1();
-				function part1()
-				{
-					x2+=1;
-					ctx.clearRect(0,0, c.width, c.height);
-					ctx.drawImage(customer, x, y, 50, 50);
-					ctx.drawImage(waiter, x2, y2, 75, 75);
-					
-					ctx.drawImage(chef, x3,y3, 50, 50);
-					
-					var test=setTimeout(part1,10);
-					if(x2==130)
-					{
-						clearInterval(test);
-						part2();
-					}
-				}
-				function part2()
-				{
-					y2+=1;
-					ctx.clearRect(0,0, c.width, c.height);
-					ctx.drawImage(customer, x, y, 50, 50);
-					ctx.drawImage(waiter, x2, y2, 75, 75);
-					 
-					ctx.drawImage(chef, x3,y3, 50, 50);
-					var test=setTimeout(part2,10);
-					if(y2==230)
-					{
-						clearInterval(test);
-					}
-				}
-			}
 			
 			function animateCustomer1()
         		{
@@ -259,7 +130,7 @@
 							x+=1;
 							ctx.clearRect(0,0, c.width, c.height);
 							ctx.drawImage(customer, x, y, 50, 50);
-							ctx.drawImage(waiter, x2, y2, 75, 75);
+							
 							ctx.drawImage(chef, x3,y3, 50, 50);
 							var test=setTimeout(part1,10);
 							
@@ -274,7 +145,7 @@
 							y+=1;
 							ctx.clearRect(0,0, c.width, c.height);
 							ctx.drawImage(customer, x, y, 50, 50);
-							ctx.drawImage(waiter, x2, y2, 75, 75);
+							
 							ctx.drawImage(chef, x3,y3, 50, 50);
 							var test=setTimeout(part2,10);
 							
@@ -295,7 +166,7 @@
 					x3+=1;
 					ctx.clearRect(0,0, c.width, c.height);
 					ctx.drawImage(customer, x, y, 50, 50);
-					ctx.drawImage(waiter, x2, y2, 75, 75);
+					
 					ctx.drawImage(chef, x3,y3, 50, 50);
 					
 					var test=setTimeout(part1, 30);
